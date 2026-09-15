79855160d69ce6d4c66103dc4e331c5b8d9e821f
Jason <4614735+Jstar269@users.noreply.github.com>
2026-09-15 13:03:14 -0400

    chore(generated): refresh public title catalog

--- src/core/generated/nk_title_catalog.h
+++ src/core/generated/nk_title_catalog.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 #define NK_TITLE_CATALOG_SCHEMA_VERSION 1
-#define NK_TITLE_CATALOG_DIGEST "0e57a28b9f420b8f"
+#define NK_TITLE_CATALOG_DIGEST "e88e34ff7d497260"
 
 typedef enum {
     NK_TITLE_KIND_RETAIL = 0,
